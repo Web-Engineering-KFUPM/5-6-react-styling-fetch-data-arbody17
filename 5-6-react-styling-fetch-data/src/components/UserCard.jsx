929b3e2a4f8ef8520b52f301aa4d93ba68376a3f
arbody <arbody.arb@gmail.com>
2025-10-15 09:07:43 +0300

    Done task 1.5

--- 5-6-react-styling-fetch-data/src/components/UserCard.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserCard.jsx
@@ -5,20 +5,22 @@ function UserCard({ user, onUserClick }) {
   return (
     <Card className="user-card">
       <Card.Body>
-        <div>
-          <div className="user-avatar">
-            {user.name.charAt(0)}
-          </div>
+        {/* Avatar */}
+        <div className="user-avatar mb-3">
+          {user.name.charAt(0)}
         </div>
 
+        {/* User Info */}
         <Card.Title>{user.name}</Card.Title>
         <Card.Text>
           <strong>Username:</strong> {user.username}<br />
           <strong>Email:</strong> {user.email}<br />
           <strong>Phone:</strong> {user.phone}
         </Card.Text>
-      {/* Buttotn*/}
-      
+
+        <Button variant="primary" onClick={() => onUserClick(user)}>
+          View Details
+        </Button>
       </Card.Body>
     </Card>
   )
